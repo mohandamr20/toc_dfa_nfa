a739115c4b8112b34b2fbfcbb27031ed4cf39130
Abdallah Shouker <81974700+AbdallahShouker@users.noreply.github.com>
2025-04-29 20:50:19 +0300

    Update and rename problem5.java to Problem5.java

---
+++ src/dfa/Problem5.java
@@ -0,0 +1,44 @@
+package dfa;
+import core.Constants;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Map;
+import static java.util.Map.entry;
+
+public class Problem5 {
+    private int[] states;
+    private int[] startState;
+    private int[] finalState;
+    private char[] alphabet;
+    private Map<Integer, int[][]> transitionTable;
+    private DFA dfa;
+    public Problem5(BufferedReader br , BufferedWriter bw) throws IOException {
+        initProblem(bw);
+        dfa.solveProblem(br,bw);
+    }
+
+    private void initProblem(BufferedWriter bw) throws IOException {
+        states = new int[]{0,1,2,3,4};
+        startState = new int[]{0};
+        finalState = new int[]{4};
+        alphabet = new char[]{'a','b'};
+        initTransitions();
+        initDFA();
+        bw.append("5").append("\n");
+    }
+
+    private void initTransitions(){
+        transitionTable = Map.ofEntries(
+               entry(0, new int[]{1, 0}),
+               entry(1, new int[]{0, 2}),
+               entry(2, new int[]{2, 2})
+        );
+    }
+    private void initDFA(){
+        dfa = new DFA(startState,states,finalState,alphabet,transitionTable);
+    }
+
+}
+
